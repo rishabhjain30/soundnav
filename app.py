abba7051c7a50bd3b841468df27a3e890f85a3b0
Rishabh <rishabhjain@email.com>
2016-04-02 20:16:15 +0530

    clean up

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('search.html')
 
+@app.route('/process')
+def process_world():
+	return render_template('process.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
