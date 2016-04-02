dd62b281c08bf3208d0df60e4ac41cd92f14506e
Rishabh <rishabhjain@email.com>
2016-04-02 23:58:11 +0530

    send_from_directory

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import send_from_directory
 
 app = Flask(__name__)
 
@@ -10,6 +11,10 @@ def hello_world():
 @app.route('/process')
 def process_world():
 	return render_template('process.html')
+
+@app.route('/src/<path:filename>')
+def send_js(filename):
+	return send_from_directory('static', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
